a8acdbc5e80cc097595eb94e78cec14e8f88760b
Austin Beeler <abeeler1@uncc.edu>
2016-03-07 03:28:57 -0500

    Bell functionality modified to have bells that are slid on a music staff

---
+++ Assets/BellSlide.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BellSlide : MonoBehaviour {
+    public static float[] xPositions = { -5.97f, -4.27f, -2.57f, -0.87f, 0.83f, 2.53f, 4.23f, 5.93f };
+
+    public AudioClip bellSound;
+
+    private byte index = 0;
+    private bool dragging = false;
+
+	// Use this for initialization
+	void OnMouseDown () {
+        dragging = true;
+	}
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (!dragging) return;
+
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+        for (int i = 0; i < xPositions.Length; i++)
+            if (index != i && Distance(mouseX, xPositions[i]) < Distance(mouseX, xPositions[index]))
+            {
+                index = (byte)i;
+                transform.position = new Vector3(xPositions[index], transform.position.y);
+                break;
+            }
+    }
+
+    float Distance(float a, float b) { return Mathf.Abs(a - b); }
+
+    public byte GetIndex() { return index; }
+
+    public bool PlaySound(int targetIndex) {
+        if (index != targetIndex) return false;
+            
+        AudioSource.PlayClipAtPoint(bellSound, Camera.main.transform.position);
+        return true;
+    }
+}
--- Assets/Scripts/Rituals/BellRitual.cs
+++ Assets/Scripts/Rituals/BellRitual.cs
@@ -1,49 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 /// <summary>
 /// Handles the logic for the rhythmic bell ritual
 /// </summary>
 public class BellRitual : Ritual
 {
-    // Duration of the entire segment in seconds
-    const float TIME_PER_SEGMENT = 6f;
-
-    // Error bounds for the note from its intended beat
-    const float ACCEPTABLE_NOTE_DISTANCE = .3f;
-
-    // The Y value each possible note will be displayed at
-    private float[] NOTE_Y_VALUES = { 1.5f, .5f, -.5f, -1.5f };
-
-    private KeyCode[] INPUTS_TO_CHECK = { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T };
-    private string[] KEY_TEXT_VALUES = { "A", "B", "C", "D", "E" };
+    public float TimePerPlaythrough = 6f; 
 
     public Vector3 meterStartPosition;
     public Vector3 meterEndPosition;
+
     public GameObject staffMeter;
-    public GameObject musicStaff;
-    public GameObject beat;
-    public GameObject correctNote;
-    public GameObject incorrectNote;
+    public GameObject[] bells;
+    public GameObject[] xPositionMarkers;
 
-    private int meterNote = 0;
-    private float elapsedTime;
-    private float[] noteXValues;
-    private byte[] notesPlayed;
-
-    private int currentBeats = 1;
-
-    public override void ShowRitual()
-    {
-        base.ShowRitual();
-        elapsedTime = 0;
-    }
+    private float elapsedTime = 0;
+    private int currentNote = 0;
 
     void Start()
     {
-        GenerateStaff(PossibleBeats.Half);
-        resetMeter();
+        BellSlide.xPositions = new float[xPositionMarkers.Length];
+        for (int i = 0; i < xPositionMarkers.Length; i++)
+            BellSlide.xPositions[i] = xPositionMarkers[i].transform.position.x;
+
+        canSubmit = true;
     }
 
     void Update()
@@ -51,91 +34,28 @@ public class BellRitual : Ritual
         if (IsClosing() || IsSubmitting())
             return;
 
-        if (Input.GetKeyDown(KeyCode.Backspace))
-            resetMeter();
-
-        // Rotate to the next beats per segment
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // When the meter has reached the end
+        if ((elapsedTime += Time.deltaTime) >= TimePerPlaythrough)
         {
-            if (++currentBeats == 5)
-                currentBeats = 1;
-            GenerateStaff((PossibleBeats)((int)Mathf.Pow(2, currentBeats) + 1));
-            resetMeter();
+            currentNote = 0;
+            elapsedTime = 0;
         }
+            
 
-        // When the segment has ended
-        if (elapsedTime < TIME_PER_SEGMENT)
-            if ((elapsedTime += Time.deltaTime) > TIME_PER_SEGMENT)
-            {
-                // If there is a missed note or no notes at all
-                if (Array.Exists(notesPlayed, n => n == 9) || !Array.Exists(notesPlayed, n => n > 0))
-                    resetMeter();
-                else
-                {
-                    elapsedTime = TIME_PER_SEGMENT;
-                    canSubmit = true;
-                }
-            }
-
-        staffMeter.transform.localPosition = Vector3.Lerp(meterStartPosition, meterEndPosition, Mathf.SmoothStep(-.05f, 1.05f, elapsedTime / TIME_PER_SEGMENT));
-        float meterX = staffMeter.transform.localPosition.x;
-        if (meterNote < noteXValues.Length - 1 && noteXValues[meterNote + 1] - meterX < meterX - noteXValues[meterNote])
-            meterNote++;
-
-        if (notesPlayed[meterNote] == 0)
-            for (int i = 0; i < INPUTS_TO_CHECK.Length && notesPlayed[meterNote] == 0; i++)
-                if (Input.GetKeyDown(INPUTS_TO_CHECK[i]))
-                {
-                    bool validNote = Mathf.Abs(meterX - noteXValues[meterNote]) < ACCEPTABLE_NOTE_DISTANCE;
-                    notesPlayed[meterNote] = (byte)(validNote ? i + 1 : 9);
-                    GameObject currentNote = GameObject.Instantiate(validNote ? correctNote : incorrectNote);
-                    currentNote.transform.parent = musicStaff.transform;
-                    currentNote.transform.localPosition = new Vector3(meterX, NOTE_Y_VALUES[i]);
-                    currentNote.transform.Find("Note Text").GetComponent<TextMesh>().text = KEY_TEXT_VALUES[i];
-                }
-
-    }
-
-    /// <summary>
-    /// Reset all notes and the current place of the meter
-    /// </summary>
-    private void resetMeter()
-    {
-        canSubmit = false;
-        elapsedTime = 0;
-        notesPlayed = new byte[notesPlayed.Length];
-        foreach (Transform t in musicStaff.transform)
-            if (t.tag == "Note")
-                GameObject.Destroy(t.gameObject);
-        meterNote = 0;
-    }
-
-    public enum PossibleBeats { Half = 3, Fourth = 5, Eigth = 9, Sixteenth = 17 }
-    /// <summary>
-    /// Clear the current staff and add the specified beats per segment
-    /// </summary>
-    /// <param name="beats">The beats to generate</param>
-    public void GenerateStaff(PossibleBeats beats)
-    {
-        notesPlayed = new byte[(int)beats - 1];
-
-        foreach (Transform t in musicStaff.transform)
-            if (t.tag == "Beat")
-                GameObject.Destroy(t.gameObject);
-
-        noteXValues = new float[(int)beats - 1];
-        for (int i = 1; i < (int)beats; i++)
+        // Move meter across staff
+        staffMeter.transform.localPosition = Vector3.Lerp(meterStartPosition, meterEndPosition, Mathf.SmoothStep(-.05f, 1.05f, elapsedTime / TimePerPlaythrough));
+        if (currentNote < 8 && staffMeter.transform.position.x > xPositionMarkers[currentNote].transform.position.x)
         {
-            GameObject currentBeat = GameObject.Instantiate(beat);
-            currentBeat.transform.parent = musicStaff.transform;
-            currentBeat.transform.localPosition = Vector3.Lerp(meterStartPosition, meterEndPosition, (float)i / (int)beats);
-            noteXValues[i - 1] = currentBeat.transform.localPosition.x;
+            foreach (GameObject b in bells) b.GetComponent<BellSlide>().PlaySound(currentNote);
+            currentNote++;
         }
+            //foreach (GameObject bell in bells.Where(b => b.GetComponent<BellSlide>().GetIndex() == currentNote))
+                //bell.GetComponent<BellSlide>().PlaySound();
     }
 
     protected override Component GetCurrentComponent()
     {
-        return new Component(Component.Type.Bell, new List<byte>(notesPlayed));
+        return new Component(Component.Type.Bell, bells.Select(b => b.GetComponent<BellSlide>().GetIndex()).ToList());
     }
 
     protected override Component.Type GetRitualType()
