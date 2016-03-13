84ae152ccae1d999bb7ee112e4c62521645905dc
Austin Beeler <abeeler1@uncc.edu>
2016-03-12 22:28:22 -0500

    ImprovedLineRenderer progress

---
+++ Assets/ImprovedLineRenderer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class ImprovedLineRenderer : MonoBehaviour {
+    public float LineWeight = .3f;
+    
+    private List<Vector3> points = new List<Vector3>();
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<int> triangles = new List<int>();
+
+    void Start()
+    {
+        GetComponent<MeshFilter>().mesh = new Mesh();
+        AddPoint(Vector3.zero);
+        AddPoint(new Vector3(0, 2));
+    }
+
+    void UpdateMesh()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+
+        if (points.Count < 2) return;
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+    public void AddPoint(Vector3 point)
+    {
+        points.Add(point);
+
+        if (points.Count == 1) return;
+
+        Vector3 lastPoint = points[points.Count - 2];
+        float angleBetween = Vector3.Angle(lastPoint, point);
+        Debug.Log(lastPoint);
+        Debug.Log(point);
+        int vertexCount = vertices.Count;
+        AddVertex(lastPoint, angleBetween + 90);
+        AddVertex(lastPoint, angleBetween - 90);
+        AddVertex(point, angleBetween + 90);
+        AddVertex(point, angleBetween - 90);
+
+        triangles.Add(vertexCount);
+        triangles.Add(vertexCount + 1);
+        triangles.Add(vertexCount + 2);
+
+        triangles.Add(vertexCount + 2);
+        triangles.Add(vertexCount + 3);
+        triangles.Add(vertexCount + 1);
+
+        UpdateMesh();
+    }
+
+    void AddVertex(Vector3 point, float angleOffset)
+    {
+        angleOffset *= Mathf.Deg2Rad;
+        vertices.Add(new Vector3(point.x + Mathf.Cos(angleOffset) * LineWeight, point.y + Mathf.Sin(angleOffset) * LineWeight, point.z));
+        Debug.Log(string.Format("{0} [{1}] => {2}", point, angleOffset, vertices[vertices.Count - 1]));
+    }
+
+    [ContextMenu("Add Random Point")]
+    void DoSomething()
+    {
+        AddPoint(new Vector3(Random.value * 2 - 1, Random.value * 2 - 1));        
+    }
+}
--- Assets/Scripts/Rituals/RuneRitual.cs
+++ Assets/Scripts/Rituals/RuneRitual.cs
@@ -9,9 +9,7 @@ public class RuneRitual : Ritual {
     List<byte> pointsConnected = new List<byte>();
     LineRenderer lineRenderer;
     Vector2 location;
-    public Color good = Color.green;
-    public Color neutral = Color.blue;
-    public Color bad = Color.red;
+    public Color good = Color.green, neutral = Color.blue, bad = Color.red;
     //int lineCount = 0;
     //int currentPosition = 0;
     float startPointDistance;
@@ -19,20 +17,12 @@ public class RuneRitual : Ritual {
     float tempDistance;
     Vector2[] pointVectors = new Vector2[9];
     public Vector2[] matchPattern = new Vector2[5];
-    Double pointPosition;
+
+    byte[] properPoints;
 
     void Start()
     {
         canSubmit = true;
-       
-        //for (int x = -1; x <= 1; x++)
-        //    pointVectors[2 + (x + 1) * 3] = new Vector2(x * GameObject.Find("Point" + i.ToString()).transform.position.x, GameObject.Find("Point" + i.ToString()).transform.position.y);
-        //for (int x = -1; x <= 1; x++)
-        //    pointVectors[1 + (x + 1) * 3] = new Vector2(x * 3, 0);
-        //for (int x = -1; x <= 1; x++)
-        //    pointVectors[0 + (x + 1) * 3] = new Vector2(x * 2, 2);
-
-       
         startPointDistance = 9999;
         nextPointDistance = 9999;
 
@@ -109,6 +99,7 @@ public class RuneRitual : Ritual {
                 if (nextPointDistance <= 0.4 && !pointsConnected.Contains((byte)i))
                 {
                     pointsConnected.Add((byte)i);
+                    UpdateColors();
                 }
             }
         }
@@ -118,15 +109,14 @@ public class RuneRitual : Ritual {
             {
                 lineRenderer.SetVertexCount(pointsConnected.Count);
                 if (pointsConnected.Count > 0)
+                {
                     pointsConnected.RemoveAt(pointsConnected.Count - 1);
+                    UpdateColors();
+                }
             }
             else
             {
                 lineRenderer.SetVertexCount(pointsConnected.Count + 1);
-            }
-            for (int i = 0; i < pointsConnected.Count; i++)
-            {
-                lineRenderer.SetPosition(i, pointVectors[pointsConnected[i]]);
             }
 
             if (Input.GetMouseButton(0))
@@ -138,6 +128,19 @@ public class RuneRitual : Ritual {
         else
         {
             lineRenderer.SetVertexCount(0);
+        }
+    }
+
+    void UpdateColors()
+    {
+        if (properPoints != null)
+            lineRenderer.SetColors(neutral, neutral);
+        else
+        {
+            bool allCorrect = pointsConnected.Count < properPoints.Length;
+            for (int i = 0; allCorrect && i < properPoints.Length; i++)
+                allCorrect = pointsConnected[i] == properPoints[i];
+            lineRenderer.SetColors(allCorrect ? good : bad, allCorrect ? good : bad);
         }
     }
 
