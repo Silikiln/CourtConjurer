015167bda4d757b564f45396bcd56d54eb1bdd3e
ZalThulu <rcopperheads11@aol.com>
2016-03-26 14:42:09 -0400

    Added new starting screen scene

---
+++ Assets/Scripts/SceneManage/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+    public string sceneToLoad;
+
+    public void loadNewScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
